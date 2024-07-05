5a2fc687a8534ba28f0deddacd642bfcf7cbdd32
Abel Sánchez <abelsandev@gmail.com>
2024-07-04 18:49:59 -0700

    Se agrego tabla y funcion para validar si los datos que entran son 1 o 0

---
+++ .VSCodeCounter/2024-07-04_18-12-25/results.md
@@ -0,0 +1,28 @@
+# Summary
+
+Date : 2024-07-04 18:12:25
+
+Directory c:\\Users\\abels\\OneDrive\\Escritorio\\proyecto-2-equipo-2\\src
+
+Total : 10 files,  736 codes, 315 comments, 31 blanks, all 1082 lines
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| TypeScript JSX | 7 | 717 | 314 | 28 | 1,059 |
+| CSS | 1 | 12 | 0 | 2 | 14 |
+| TypeScript | 2 | 7 | 1 | 1 | 9 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 10 | 736 | 315 | 31 | 1,082 |
+| . (Files) | 4 | 43 | 1 | 8 | 52 |
+| chi | 6 | 693 | 314 | 23 | 1,030 |
+| chi\\components | 2 | 39 | 0 | 6 | 45 |
+| chi\\helpers | 1 | 7 | 0 | 0 | 7 |
+| chi\\pages | 3 | 647 | 314 | 17 | 978 |
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- src/chi/pages/PutDataManually.tsx
+++ src/chi/pages/PutDataManually.tsx
@@ -1,10 +1,11 @@
-import { commonColors, Input } from "@nextui-org/react";
+import { Button, Input, Tooltip } from "@nextui-org/react";
 import { ChangeEvent, useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 //*Iterfaz de como tiene que ser los datos de cada item
 interface ItemFormat {
   nombre: string;
-  datos: number[];
+  datos: string;
 }
 //*Interfaz para darle el formato a los 8 items permitidos
 interface ItemsData {
@@ -21,16 +22,34 @@ interface ItemsData {
 const PutDataManually = () => {
   //*Este useState se va a utilizar para poder almacenar el nombre y los dato binarios de los items
   const [items, setItems] = useState({
-    item1: { nombre: "", datos: [] },
-    item2: { nombre: "", datos: [] },
-    item3: { nombre: "", datos: [] },
-    item4: { nombre: "", datos: [] },
-    item5: { nombre: "", datos: [] },
-    item6: { nombre: "", datos: [] },
-    item7: { nombre: "", datos: [] },
-    item8: { nombre: "", datos: [] },
+    item1: { nombre: "", datos: "" },
+    item2: { nombre: "", datos: "" },
+    item3: { nombre: "", datos: "" },
+    item4: { nombre: "", datos: "" },
+    item5: { nombre: "", datos: "" },
+    item6: { nombre: "", datos: "" },
+    item7: { nombre: "", datos: "" },
+    item8: { nombre: "", datos: "" },
   });
-
+  //*Este estado es para poder saber si items tiene valores
+  const [state, setState] = useState<boolean>(false);
+  //*Este useEfffect es para poder cambiar el state en el caso de que algun item en el apartado nombre tiene valor de ser asi se activara la tabla
+  useEffect(() => {
+    if (
+      items.item1.nombre ||
+      items.item2.nombre ||
+      items.item3.nombre ||
+      items.item4.nombre ||
+      items.item5.nombre ||
+      items.item6.nombre ||
+      items.item7.nombre ||
+      items.item8.nombre
+    ) {
+      setState(true);
+    } else {
+      setState(false);
+    }
+  }, [items]);
   //*Esta funcion es para almacenar el valor nombre de los items
   const handleOnChangeItem = (e: ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target;
@@ -42,19 +61,65 @@ const PutDataManually = () => {
       },
     }));
   };
-  console.log(items);
-
+  const clearData = (): void => {
+    setItems({
+      item1: { nombre: "", datos: "" },
+      item2: { nombre: "", datos: "" },
+      item3: { nombre: "", datos: "" },
+      item4: { nombre: "", datos: "" },
+      item5: { nombre: "", datos: "" },
+      item6: { nombre: "", datos: "" },
+      item7: { nombre: "", datos: "" },
+      item8: { nombre: "", datos: "" },
+    });
+  };
+  const handleOnChangeItemValues = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    const lon = value.length;
+    let insideState: boolean = true;
+    //*Este for dentro tiene un if el cual valida que los datos sean solo 1 y 0 y el " "
+    for (let i: number = 0; i < lon; i++) {
+      if (
+        (value[i] === "1" && value[i + 1] === "1") ||
+        (value[i] === "1" && value[i + 1] === "0") ||
+        (value[i] === "0" && value[i + 1] === "1") ||
+        (value[i] === "0" && value[i + 1] === "0")
+      ) {
+        insideState = false;
+        break;
+      } else if (value[i] === "0" || value[i] === "1" || value[i] === " ") {
+        console.log("permitido");
+      } else {
+        insideState = false;
+        break;
+      }
+    }
+    if (insideState) {
+      setItems((prevItems) => ({
+        ...prevItems,
+        [name]: {
+          ...prevItems[name as keyof ItemsData],
+          datos: value,
+        },
+      }));
+    } else {
+      toast.error("Solo se permiten los datos 1 o 0");
+    }
+  };
   return (
     <>
-      <div className="container mx-auto grid grid-cols-2 gap-5">
-        <div>
-          <h1>Poner data de forma manual</h1>
+      <div className="container mx-auto grid grid-cols-2 gap-5 ">
+        <div className="mt-5">
+          <h1 className="mb-5 font-thin text-3xl">
+            Ingresa los el nombre de los items
+          </h1>
           {/**Poner el nombre a los items*/}
           <div className="grid grid-cols-8 gap-1">
             <Input
               name="item1"
               onChange={(e) => handleOnChangeItem(e)}
               type="text"
+              value={items.item1.nombre}
               variant={"underlined"}
               label="Item1"
             />
@@ -62,6 +127,7 @@ const PutDataManually = () => {
               name="item2"
               onChange={(e) => handleOnChangeItem(e)}
               type="text"
+              value={items.item2.nombre}
               variant={"underlined"}
               label="Item2"
             />
@@ -69,6 +135,7 @@ const PutDataManually = () => {
               name="item3"
               onChange={(e) => handleOnChangeItem(e)}
               type="text"
+              value={items.item3.nombre}
               variant={"underlined"}
               label="Item3"
             />
@@ -76,6 +143,7 @@ const PutDataManually = () => {
               name="item4"
               onChange={(e) => handleOnChangeItem(e)}
               type="text"
+              value={items.item4.nombre}
               variant={"underlined"}
               label="Item4"
             />
@@ -83,6 +151,7 @@ const PutDataManually = () => {
               name="item5"
               onChange={(e) => handleOnChangeItem(e)}
               type="text"
+              value={items.item5.nombre}
               variant={"underlined"}
               label="Item5"
             />
@@ -90,6 +159,7 @@ const PutDataManually = () => {
               name="item6"
               onChange={(e) => handleOnChangeItem(e)}
               type="text"
+              value={items.item6.nombre}
               variant={"underlined"}
               label="Item6"
             />
@@ -97,6 +167,7 @@ const PutDataManually = () => {
               name="item7"
               onChange={(e) => handleOnChangeItem(e)}
               type="text"
+              value={items.item7.nombre}
               variant={"underlined"}
               label="Item7"
             />
@@ -104,28 +175,181 @@ const PutDataManually = () => {
               name="item8"
               onChange={(e) => handleOnChangeItem(e)}
               type="text"
+              value={items.item8.nombre}
               variant={"underlined"}
               label="Item8"
             />
           </div>
+          <div className="my-5 fixed top-1 left-6">
+            <Button color="danger" size="lg" onClick={clearData}>
+              Reset
+            </Button>
+          </div>
+          {/**Estos input son para poner los datos de las columnas */}
+          <div className="my-5">
+            <div className="flex items-center gap-2">
+              <Tooltip
+                size="lg"
+                content="Para poner los datos se tienen que separar por espacios, por ejemplo: 1 0 1 0 1 0 0 1 0 1 0 1 0 0 1 0"
+                className="w-[20rem] font-bold text-xl"
+                color="warning"
+              >
+                <div className="w-10">
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM12 7.75C11.3787 7.75 10.875 8.25368 10.875 8.875C10.875 9.28921 10.5392 9.625 10.125 9.625C9.71079 9.625 9.375 9.28921 9.375 8.875C9.375 7.42525 10.5503 6.25 12 6.25C13.4497 6.25 14.625 7.42525 14.625 8.875C14.625 9.58584 14.3415 10.232 13.883 10.704C13.7907 10.7989 13.7027 10.8869 13.6187 10.9708C13.4029 11.1864 13.2138 11.3753 13.0479 11.5885C12.8289 11.8699 12.75 12.0768 12.75 12.25V13C12.75 13.4142 12.4142 13.75 12 13.75C11.5858 13.75 11.25 13.4142 11.25 13V12.25C11.25 11.5948 11.555 11.0644 11.8642 10.6672C12.0929 10.3733 12.3804 10.0863 12.6138 9.85346C12.6842 9.78321 12.7496 9.71789 12.807 9.65877C13.0046 9.45543 13.125 9.18004 13.125 8.875C13.125 8.25368 12.6213 7.75 12 7.75ZM12 17C12.5523 17 13 16.5523 13 16C13 15.4477 12.5523 15 12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17Z"
+                        fill="#006FEE"
+                      ></path>{" "}
+                    </g>
+                  </svg>
+                </div>
+              </Tooltip>
+              <h1 className=" font-thin text-sm">
+                Nota importante antes de poner datos manualmente
+              </h1>
+            </div>
+
+            {items.item1.nombre && (
+              <Input
+                name="item1"
+                onChange={(e) => handleOnChangeItemValues(e)}
+                type="text"
+                value={items.item1.datos}
+                variant={"underlined"}
+                label="Datos de Item1 "
+              />
+            )}
+            {items.item2.nombre && (
+              <Input
+                name="item2"
+                onChange={(e) => handleOnChangeItemValues(e)}
+                type="text"
+                value={items.item2.datos}
+                variant={"underlined"}
+                label="Datos de Item2"
+              />
+            )}
+
+            {items.item3.nombre && (
+              <Input
+                name="item3"
+                onChange={(e) => handleOnChangeItemValues(e)}
+                type="text"
+                value={items.item3.datos}
+                variant={"underlined"}
+                label="Datos de Item3"
+              />
+            )}
+
+            {items.item4.nombre && (
+              <Input
+                name="item4"
+                onChange={(e) => handleOnChangeItemValues(e)}
+                type="text"
+                value={items.item4.datos}
+                variant={"underlined"}
+                label="Datos de Item4"
+              />
+            )}
+
+            {items.item5.nombre && (
+              <Input
+                name="item5"
+                onChange={(e) => handleOnChangeItemValues(e)}
+                type="text"
+                value={items.item5.datos}
+                variant={"underlined"}
+                label="Datos de Item5"
+              />
+            )}
+
+            {items.item6.nombre && (
+              <Input
+                name="item6"
+                onChange={(e) => handleOnChangeItemValues(e)}
+                type="text"
+                value={items.item6.datos}
+                variant={"underlined"}
+                label="Datos de Item6"
+              />
+            )}
+
+            {items.item7.nombre && (
+              <Input
+                name="item7"
+                onChange={(e) => handleOnChangeItemValues(e)}
+                type="text"
+                value={items.item7.datos}
+                variant={"underlined"}
+                label="Datos de Item7"
+              />
+            )}
+
+            {items.item8.nombre && (
+              <Input
+                name="item8"
+                onChange={(e) => handleOnChangeItemValues(e)}
+                type="text"
+                value={items.item8.datos}
+                variant={"underlined"}
+                label="Datos de Item8"
+              />
+            )}
+          </div>
         </div>
-        {/**En esta parte se mostraran los dotos en una tabla */}
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>{items.item1.nombre}</th>
-                <th>{items.item2.nombre}</th>
-                <th>{items.item3.nombre}</th>
-                <th>{items.item4.nombre}</th>
-                <th>{items.item5.nombre}</th>
-                <th>{items.item6.nombre}</th>
-                <th>{items.item7.nombre}</th>
-                <th>{items.item8.nombre}</th>
-              </tr>
-            </thead>
-          </table>
+        {/**En esta parte se mostraran los datos en una tabla */}
+        <div className="mt-5">
+          {state && (
+            <>
+              <h1 className="mb-5 font-thin text-3xl">Datos</h1>
+              <table className="min-w-full bg-white border-gray-200 shadow-md rounded-lg overflow-hidden">
+                <thead className="bg-gray-100">
+                  <tr>
+                    <th className="border-b-2 border-gray-300 py-2 px-4 text-left text-sm font-semibold text-gray-700">
+                      {items.item1.nombre}
+                    </th>
+                    <th className="border-b-2 border-gray-300 py-2 px-4 text-left text-sm font-semibold text-gray-700">
+                      {items.item2.nombre}
+                    </th>
+                    <th className="border-b-2 border-gray-300 py-2 px-4 text-left text-sm font-semibold text-gray-700">
+                      {items.item3.nombre}
+                    </th>
+                    <th className="border-b-2 border-gray-300 py-2 px-4 text-left text-sm font-semibold text-gray-700">
+                      {items.item4.nombre}
+                    </th>
+                    <th className="border-b-2 border-gray-300 py-2 px-4 text-left text-sm font-semibold text-gray-700">
+                      {items.item5.nombre}
+                    </th>
+                    <th className="border-b-2 border-gray-300 py-2 px-4 text-left text-sm font-semibold text-gray-700">
+                      {items.item6.nombre}
+                    </th>
+                    <th className="border-b-2 border-gray-300 py-2 px-4 text-left text-sm font-semibold text-gray-700">
+                      {items.item7.nombre}
+                    </th>
+                    <th className="border-b-2 border-gray-300 py-2 px-4 text-left text-sm font-semibold text-gray-700">
+                      {items.item8.nombre}
+                    </th>
+                  </tr>
+                </thead>
+              </table>
+            </>
+          )}
         </div>
+        <ToastContainer />
       </div>
     </>
   );
